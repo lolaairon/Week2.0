e4616c952e6f61dab1b917b7a59fd95bba167295
unknown <lolaairon@gmail.com>
2022-05-12 15:02:49 +0200

    Subo carpeta con los ejercicios de la rama 1

---
+++ POO/person.js
@@ -0,0 +1,14 @@
+
+const { Contacts } = require('./contacts.js');
+const { Persona } = require('./retos.js');
+
+let computador = new Persona
+
+computador.calcularIMC()
+computador.calcularEdad()
+computador.printAll()
+computador.printHobbies()
+
+
+//console.log(con);
+//console.log(persona2.hobbies)
